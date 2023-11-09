4bc806551c9b7f868fe8cd15e093dbf5f497ef93
Munaviola <munaviola5@gmail.com>
2023-11-09 14:24:53 +0100

    ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.portfolio.app',
+  appName: 'ionic-portfolio-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
