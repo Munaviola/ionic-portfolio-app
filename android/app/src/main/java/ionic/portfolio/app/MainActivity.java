0e7f336f3cd970dd21e1b7644a68bb7c83263781
Munaviola <munaviola5@gmail.com>
2023-11-09 14:25:37 +0100

    ionic portfolio app

---
+++ android/app/src/main/java/ionic/portfolio/app/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.portfolio.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+// import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    // IonicModule,
+    FormsModule,
+  ],
 })
 export class HomePage {
+  userData = {
+    user: '',
+  };
+
   constructor() {}
+
+  submit() {
+    const formData = this.userData;
+    const message = `Name: ${formData.user}`;
+    alert(message);
+  }
 }
